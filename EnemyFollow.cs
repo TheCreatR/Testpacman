ed6ca862f0578972c8d5ab56a1ed2dd285c7a1b3
TheCreatR <52386719+TheCreatR@users.noreply.github.com>
2020-01-22 01:47:48 -0500

    Add files via upload

---
+++ EnemyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float dist;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < dist ){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);}
+    }
+}
